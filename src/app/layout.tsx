8c445aa38b488a91da9837b5089bb4c582fcd07d
Aman <chhetryaman3@gmail.com>
2025-07-24 19:40:08 +0530

    added @vercel/analytics in the code!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const fontSans = FontSans({
@@ -66,6 +67,7 @@ export default function RootLayout({
           <TooltipProvider delayDuration={0}>
             {children}
             <Navbar />
+            <Analytics />
             <SpeedInsights />
 
           </TooltipProvider>
