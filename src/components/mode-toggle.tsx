45a18e941a0394e8cfdc898b63e2d69d5e381847
Aman <chhetryaman3@gmail.com>
2026-02-16 19:00:57 +0530

    fix: add forwardRef to ModeToggle component

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,20 +3,25 @@
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import { forwardRef } from "react";
 
-export function ModeToggle() {
+export const ModeToggle = forwardRef<HTMLButtonElement>((props, ref) => {
   const { theme, setTheme } = useTheme();
 
   return (
     <Button
+      ref={ref}
       variant="ghost"
       type="button"
       size="icon"
       className="px-2"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      {...props}
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
       <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
     </Button>
   );
-}
+});
+
+ModeToggle.displayName = "ModeToggle";
