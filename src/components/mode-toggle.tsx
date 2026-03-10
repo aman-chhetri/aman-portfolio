c53a2da6b35dc33af4d6cf28fe0f56fd238859be
Aman <chhetryaman3@gmail.com>
2026-03-10 13:29:21 +0530

    fix: ToggleMode-button dark-light ✅

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,25 +3,20 @@
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-import { forwardRef } from "react";
 
-export const ModeToggle = forwardRef<HTMLButtonElement>((props, ref) => {
+export function ModeToggle() {
   const { theme, setTheme } = useTheme();
 
   return (
     <Button
-      ref={ref}
       variant="ghost"
       type="button"
       size="icon"
       className="px-2"
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      {...props}
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
       <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
     </Button>
   );
-});
-
-ModeToggle.displayName = "ModeToggle";
+}
